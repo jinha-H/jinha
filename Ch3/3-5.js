29cfa8cbe35e509176b603b612a12dfaf88aecd1
jinha-H <jhjs9335@gmail.com>
2024-04-29 14:33:38 +0900

    global variables & global objects(3)-about deleting

--- Ch3/3-5.js
+++ Ch3/3-5.js
@@ -1,15 +1,15 @@
 var a = 1;
-delete window.a; // false
-console.log(a, window.a, this.a); // 1 1 1
+delete global.a; // false
+console.log(a, global.a, this.a); // 1 1 1
 
 var b = 2;
 delete b; // false
-console.log(b, window.b, this.b); // 2 2 2
+console.log(b, global.b, this.b); // 2 2 2
 
-window.c = 3;
-delete window.c; // true
-console.log(c, window.c, this.c); // Uncaught ReferenceError: c is not defined
+global.c = 3;
+delete global.c; // true
+console.log(c, global.c, this.c); // Uncaught ReferenceError: c is not defined
 
-window.d = 4;
+global.d = 4;
 delete d; // true
-console.log(d, window.d, this.d); // Uncaught ReferenceError: d is not defined
+console.log(d, global.d, this.d); // Uncaught ReferenceError: d is not defined
