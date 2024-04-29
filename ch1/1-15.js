1437170dbdbb811308a3c46689898e6f71ba261d
jinha-H <jhjs9335@gmail.com>
2024-04-29 12:11:18 +0900

    user.urls property (불변객체)

---
+++ ch1/1-15.js
@@ -0,0 +1,25 @@
+var copyObject = function(target) {
+    var result = {};
+    for (var prop in target) {
+      result[prop] = target[prop];
+    }
+    return result;
+  };
+  
+  var user = {
+    name: 'Jinha',
+    urls: {
+      portfolio: 'https://github.com/jinha-H',
+      blog: 'https://blog.naver.com/jinha9335',
+      facebook: 'http://facebook.com/abc',
+    },
+  };
+  
+  var user2 = copyObject(user);
+  user2.urls = copyObject(user.urls);
+  
+  user.urls.portfolio = 'http://portfolio.com';
+  console.log(user.urls.portfolio === user2.urls.portfolio); // false
+  
+  user2.urls.blog = '';
+  console.log(user.urls.blog === user2.urls.blog); // false
