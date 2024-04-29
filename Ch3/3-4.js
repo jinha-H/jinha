5bdd508d9c6b7abefd302d1abf8152efb4dc19b3
jinha-H <jhjs9335@gmail.com>
2024-04-29 14:31:33 +0900

    global variables & global objects(2)

--- Ch3/3-4.js
+++ Ch3/3-4.js
@@ -1,9 +1,9 @@
 var a = 1;
-window.b = 2;
+global.b = 2;
 console.log(a, global.a, this.a); // 1 1 1
 console.log(b, global.b, this.b); // 2 2 2
 
-window.a = 3;
+global.a = 3;
 b = 4;
 console.log(a, global.a, this.a); // 3 3 3
 console.log(b, global.b, this.b); // 4 4 4
